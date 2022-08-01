b822ba582824b36778d0b77db2cc3b51763e71d9
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-08-01 19:54:49 +0600

    slice redux

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,5 +1,14 @@
 import { createSlice} from '@reduxjs/toolkit';
 
 export const userSlice = createSlice({
-    name:"user"
+    name:"user",
+    initialState:{
+        name:"john",
+        email:"john@gmail.com"
+    },
+    reducers:{
+        update:(state,action) => {
+        
+        }
+    }
 })
